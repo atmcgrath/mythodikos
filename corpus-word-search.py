04d34d397536ef258a11922e1131db2ec2a71045
Alice McGrath <atmcgrath@gmail.com>
2020-01-10 17:30:54 -0500

    revised documentation

--- corpus-word-search.py
+++ corpus-word-search.py
@@ -5,31 +5,31 @@ Created on Thu Jan  9 15:41:03 2020
 
 Code for mythodikos project
 1. Iterates through corpus
-2. Opens file, reads file, turns raw text into nltk text object
-3. Searches a text for keyword and returns a slice 5 words before and after keyword 
+2. Opens text file, reads file, turns raw text into nltk text object
+3. Searches a text for keyword and returns a slice 5 words before and after keyword
 
 Next steps:
 - Find word matches with dictionary list (placenames)
 - Write matches to csv with filename
 - Turn chunks into functions
 
-@author: amcgrath1
+NB: I made some untested changes to clean this up 1/10
+
+@author: atmcgrath
 """
 
 import nltk # natural language toolkit - http://www.nltk.org/
-import re
+# import re
 # import pprint
 from nltk import word_tokenize
-
 # import os
-
 
 # =============================================================================
 # Iterates through files and subfolders in the directory contaning the corpus
 # =============================================================================
 
 """
-corpusdir = "/Users/amcgrath1/cltk_data/latin/text/latin_text_latin_library"
+corpusdir = "~/cltk_data/latin/text/latin_text_latin_library"
 
 for root, dirs, files in os.walk(corpusdir):
     for filename in files:
@@ -41,7 +41,7 @@ for root, dirs, files in os.walk(corpusdir):
 # Processes single text file into nltk text object (a list of words, among other things)
 # =============================================================================
 
-infile = "/Users/amcgrath1/cltk_data/latin/text/latin_text_latin_library/agnes.txt"
+infile = "~/cltk_data/latin/text/latin_text_latin_library/agnes.txt"
 
 raw = open(infile).read() # stores contents of file
 tokens = word_tokenize(raw) # tokenizes raw text (list of words)
