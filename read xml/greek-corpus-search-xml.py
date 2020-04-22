da9750c23beadd372c6c3544608d583d32548186
Alice McGrath <atmcgrath@gmail.com>
2020-04-22 17:45:31 -0400

    fixed citation function

--- read xml/greek-corpus-search-xml.py
+++ read xml/greek-corpus-search-xml.py
@@ -36,89 +36,94 @@ import csv
 # Functions
 # =====================================================================
 
-def get_citation(line):
-	cite_list = []
-	sections = [parent.attrs for parent in line.parents if parent.name == 'div'] # Stephanus numbers (i.e. Plato) would be more accurate in some cases, but these are 'milestone' parents and non-priority
-	for s in sections: # for attritubutes of 'div'
-		if s['type'] == 'textpart': # only look at textpart 'div'
-			try:
-				cite_list.append(s['subtype']+s['n']) #printing subtype tells us exactly which parts of the citation are being returned -- comment out later
-			except KeyError:
-				continue
-		elif s['type'] == 'poem': # to get the number for a poem in an author's work
-			try:
-				cite_list.append(s['type']+s['n'])
-			except KeyError:
-				continue
-	cite_list.reverse() # make order book - chapter - section
+def get_section(line):
+    cite_list = []
+    for parent in line.parents:
+        if parent.name == 'div':
+            if parent.attrs['type'] == 'textpart':
+                try:
+                    cite_list.append(parent.attrs['subtype']+parent.attrs['n']) #printing subtype tells us exactly which parts of the citation are being returned -- comment out later
+                except KeyError:
+                    continue
+            else: continue
+        elif 'div' in parent.name:
+            try:
+                cite_list.append(parent.attrs['type']+parent.attrs['n'])
+            except KeyError:
+                continue
+    cite_list.reverse() # make order book - chapter - section
+    citation = '.'.join(cite_list) # make citation format book.chapter.section.line
+    return citation
+# 4/22 - I cleaned up the citation function (above) It now gets section info for 114 out of 126
 
-	textchunk = line.parent # m.parent == the exact section of .xml text in which the match is found (could be a paragraph or a line)
-	for chunk in textchunk:
-		if textchunk.name == 'l': # for line number of match
-			try:
-				cite_list.append('line'+textchunk['n'])
-			except KeyError:
-				continue
-		elif textchunk.name == 'lb': #this should account for <lb n='#' rend="displayNum"/> line citation in Callimachus files (tlg0533.tlg017), but does not.  Why?
-			try:
-				cite_list.append('line'+textchunk['n'])
-			except KeyError:
-				continue
-
-			# some files are encoded with line numbers for every 5 lines of text.  How to address this?
-
-	citation = '.'.join(cite_list) # make citation format book.chapter.section.line
-	return citation
+def get_linenum(line): # separated functions to test
+    textchunk = line.parent # m.parent == the exact section of .xml text in which the match is found (could be a paragraph or a line)
+    line_num = ''
+    for chunk in textchunk:
+        if textchunk.name == 'l' or 'lb': # for line number of match
+            try:
+                line_num = textchunk.name + textchunk['n']
+            except KeyError:
+                continue
+        else:
+            continue
+    return line_num
 
 """
 def get_context(line):
-	context = []
-	if m.parent.t == 'l': #or <lb/>
-		prev_line = matchchunk.previous_sibling.previous_sibling.string
-		next_line = matchchunk.next_sibling.next_sibling.string
-		context = prev_line + ' ' + m + ' ' + next_line # this returns text as a full string, rather than as three lists
-	elif m.parent.t == 'p': # for paragraphs consider using cltk / nltk sentence tokenize to return the match sentence
-		context = .... # is there a way to have the program return the text surrounding the match up to the nearest punctuation mark?
+    context = []
+    if m.parent.t == 'l': #or <lb/>
+        prev_line = matchchunk.previous_sibling.previous_sibling.string
+        next_line = matchchunk.next_sibling.next_sibling.string
+        context = prev_line + ' ' + m + ' ' + next_line # this returns text as a full string, rather than as three lists
+    elif m.parent.t == 'p': # for paragraphs consider using cltk / nltk sentence tokenize to return the match sentence
+        context = .... # is there a way to have the program return the text surrounding the match up to the nearest punctuation mark?
 """
 
 # =====================================================================
 # Program
 # =====================================================================
 
-greekcorpusdir = "/Users/stellafritzell/mythodikos/canonical-greekLit-master"
-outfile = "/Users/stellafritzell/mythodikos/corpus-test-4-21.csv"
+greekcorpusdir = "/Users/amcgrath1/classics/canonical-greekLit-master"
+outfile = "/Users/amcgrath1/classics/test-4-22-c.csv"
 
 persondict = {'Atalanta': [r'\bἈταλάντ'],
-			'Arion': [r'\bἈρίων', r'\bἈρίον']
-			} # Compile in reference to LIMC
+            'Arion': [r'\bἈρίων', r'\bἈρίον']
+            } # Compile in reference to LIMC
 
 placedict = {} # Compile in reference to Barinton Atlas of the Ancient World
 
 # build .csv with desired metadata as column headers
 with open(outfile, 'w') as z:
-	f = csv.writer(z)
-	f.writerow(['person', 'title', 'author', 'citation', 'match', 'context', 'filename'])
+    f = csv.writer(z)
+    f.writerow(['person', 'title', 'author', 'section', 'line',
+        'match', 'context', 'length', 'filename'])
 
-	for root, dirs, files in os.walk(greekcorpusdir):
-		for file in files:
-			if fnmatch.fnmatch(file, '*grc*.xml'):
-				infile = root+'/'+str(file) # specifices greek text files for search
+    for root, dirs, files in os.walk(greekcorpusdir):
+        for file in files:
+            if fnmatch.fnmatch(file, '*grc*.xml'):
+                infile = root+'/'+str(file) # specifices greek text files for search
 
-				with open(infile) as x:
-					soup = BeautifulSoup(x, features='lxml')
+                with open(infile) as x:
+                    soup = BeautifulSoup(x, features='lxml')
 
-					text_title = soup.title.string
-					if soup.author:
-						text_author = soup.author.string
-					else: text_author = ''
+                    text_title = soup.title.string
+                    if soup.author:
+                        text_author = soup.author.string
+                    else: text_author = ''
 
-					for key in persondict:
-						terms = persondict[key] # value list for each key
-						for t in terms: # each regex value in value lists
-							matches = soup.find_all(string=re.compile(t))
+                    for key in persondict:
+                        terms = persondict[key] # value list for each key
+                        for t in terms: # each regex value in value lists
+                            matches = soup.find_all(string=re.compile(t))
 
-							for m in matches: # m = entire matching string
-								citation = get_citation(m) # applies citation formula to each match
-								# context = get_context(m)
+                            for m in matches: # m = entire matching string
+                                section = get_section(m) # applies citation formula to each match
+                                line = get_linenum(m)
+                                context = m.replace('\n', ' ') # strips newline characters
+                                length = len(context) # number of characters in context
+                                # context = get_context(m)
 
-								f.writerow([key, text_title, text_author, citation, t, m, file]) #write defined variables for each match to .csv
+                                f.writerow([
+                                        key, text_title, text_author, section, line,
+                                        t, context, length, file]) #write defined variables for each match to .csv
