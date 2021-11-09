07e38ed5ad6623910509bcc8d090828191c1f759
sfritzell <55812027+sfritzell@users.noreply.github.com>
2021-11-09 14:05:48 -0500

    add function to get alt names

--- data/pleiades_clean.py
+++ data/pleiades_clean.py
@@ -6,13 +6,12 @@ Code to clean Pleiades data file for use in the Mythodikos project
 1. Opens Pleiades json file
 2. Iterates through file data
 3. Records desired data for each entry with of a particular place type
+4. Fixed issue with reducplication in original code (#out) by adding function -- why did this fix?
 
 Next Steps:
-1. Address possible reduplication of some results - are these repeat entries in the original file? Is it an issue with the search?
-2. Cross reference with Greek corpus search terms
-3. Cross reference with Greek corpus search results
-4. Expand place types, possibly to include: 'river', 'plain', 'pass', 'resevoir', 'cave', 'cape', 'hill', 'forest', 'mountain', 'island', 'water-open', 'province', 'lake', ...
-5. Write function for expanded place types (if 1st item in list, return TRUE, elif 2nd item, ect.)
+1. Cross reference with Greek corpus search terms
+2. Cross reference with Greek corpus search results
+3. Expand place types, possibly to include: 'river', 'plain', 'pass', 'resevoir', 'cave', 'cape', 'hill', 'forest', 'mountain', 'island', 'water-open', 'province', 'lake', ...
 
 @author: sfritzell
 ''' 
@@ -25,7 +24,7 @@ import json
 # =====================================================================
 # Functions
 # =====================================================================
-def select_type(types):
+def select_type(types): #to look at Pleiades items only of the specified types
 	place_types = ['settlement', 'region']
 	relevant = False
 	for item in place_types:
@@ -36,6 +35,14 @@ def select_type(types):
 			continue
 	return relevant
 
+def get_names(name_info): #to get a list of alternate names for desired items
+	romanized = []
+	for n in name_info:
+		item = n['romanized']
+		romanized.append(item)
+	return romanized
+
+
 # =====================================================================
 # Program
 # =====================================================================
@@ -43,7 +50,7 @@ def select_type(types):
 infile = "/Users/stellafritzell/mythodikos/pleiades-places-latest.json"
 # web url: http://atlantides.org/downloads/pleiades/json/pleiades-places-latest.json.gz
 
-outfile = "/Users/stellafritzell/mythodikos/clean-pleiades-wfunc.json"
+outfile = "/Users/stellafritzell/mythodikos/clean-pleiades.json"
 
 with open(outfile, 'w') as z:
 	with open(infile) as x:
@@ -61,21 +68,15 @@ with open(outfile, 'w') as z:
 		'''
 
 		for d in data["@graph"]:
+			typ = d['placeTypes'] #type of place represented by entry
 			name = d['title'] #common use (?) name of place
 			pl_id = d['id'] #pleiades id number
 			gps = d['reprPoint'] #representative lat-long coordinates
-			#alt_names = d['names'] #this doesn't seem like useful information right now (it has a lot of extra stuff)
-			typ = d['placeTypes'] #type of place represented by entry
-			'''
-			if "settlement" in typ:
-				ptyp = "settlement"
-				data = {name: {'location type': typ, 'Pleiades id': pl_id, 'coordinates': gps, 'place type': ptyp}}
-			elif "region" in typ:
-				ptyp = "region"
-				data = {name: {'location type': typ, 'Pleiades id': pl_id, 'coordinates': gps, 'place type': ptyp}}
-			'''
+			alt_names = d['names'] #returns EVERYTHING under 'names', mostly non-essential
+
 			if select_type(typ) == True:
-				data = {name: {'location type': typ, 'Pleiades id': pl_id, 'coordinates': gps}}
+				name_list = get_names(alt_names)
+				data = {name: {'location type': typ, 'Pleiades id': pl_id, 'coordinates': gps, 'other names': name_list}}
 
 				json.dump(data, z, indent=4) #print results in redable format to json outfile
 
