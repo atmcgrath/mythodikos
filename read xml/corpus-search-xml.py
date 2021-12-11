2d249567c2c286426c3bbe6f61443eccd9ab2922
sfritzell <55812027+sfritzell@users.noreply.github.com>
2021-12-11 08:43:53 -0500

    12/11 corpus search update

--- read xml/corpus-search-xml.py
+++ read xml/corpus-search-xml.py
@@ -6,14 +6,14 @@ Code for Mythodikos Project:
 1. iterates through corpus
 2. opens file, parses file, finds pattern matches with regex values & keys in a dictionary
 3. locates secondary pattern matches with regex values within initial search results
-4. for each match: records latinized person name (key), place name, title & author of text (needs some refinement), citation (with some issues), text context, context length and file name
-5. writes information to .csv (write to .json commented out) (NOTE: .json searches / writes much faster than .csv)
+4. for each match: records latinized person name (key), place name, geodata, title & author of text (needs some refinement), citation (with some issues), text context, context length and file name
+5. writes information to .json
 
 Next Steps:
-1. clean up citation extraction, author and title
-2. attach Pleiades IDs/geo corrdinates to places in search results (read Pleiades data .json - write to mythodikos data .json) -- use Pandas
-3. make persondict and placedict idependent (json?) files that are called by the code -- useful when expanding dataset
-4. write to download/access most recent Perseus corpus and Pleiades file
+1. combine dictionaries (citations & json info) at the appropriate level - citations need to appear under "properties" for each object
+2. make result data format / file structure appropriate for Leaflet layers
+3. clean up citation extraction, author and title (prioritize other "Next Steps" first)
+4. write to download/access most recent Perseus corpus and Pleiades file (nice to have, save for later with expanded dataset)
 
 @author: sfritzell
 adapted from: search-xml-greek.py
@@ -27,44 +27,14 @@ import os # to search file directory
 import fnmatch # to specify files
 import re # to use regex functions to search keywords
 from bs4 import BeautifulSoup # to read xml files
-#import csv # to write search results to csv
 import json # to write search results to json
+from person_file import persondict # to read the person search terms from seperate file
+from place_file import placedict # to read the place search terms from seperate file
+from collections import	defaultdict # to create new entry based on unique id
 
 # =====================================================================
 # Functions
 # =====================================================================
-
-def get_section(line): #function to get the section citation info from xml
-	cite_list = [] #creates empty list for citation information
-	for parent in line.parents: #for parents of keyword match
-		if parent.name == 'div': #if the parent is a div
-			if parent.attrs['type'] == 'textpart': #and if the div has textpart as an attibute
-				try:
-					cite_list.append(parent.attrs['subtype']+parent.attrs['n']) #grab the subtype (book, chapter, section) and the number
-				except KeyError: #if this doesn't work for one instance, keep going
-					continue
-			else: continue #if the div doesn't have this attribute, keep going
-		elif 'div' in parent.name: #if the the xml encoding is differnt, div might be in the parent name
-			try:
-				cite_list.append(parent.attrs['type']+parent.attrs['n']) #in that case, grab the type and number
-			except KeyError:
-				continue
-	cite_list.reverse() #make the citation order: book,chapter,section
-	citation = '.'.join(cite_list) #make format: book.chapter.section
-	return citation #print the citation
-
-def get_linenum(line): #separated from get_section to test, function to get the line number citation info from xml
-	textchunk = line.parent #the exact section of .xml text in which the match is found (could be a paragraph or a line)
-	line_num = '' #create an empty variable for the line number
-	for chunk in textchunk: #for each element in textchunk
-		if textchunk.name == 'l' or 'lb': #if the textchunk is marked as a line
-			try:
-				line_num = textchunk.name + ' ' + textchunk['n'] #line number is "line"+number
-			except KeyError: #if this doesn't work for one instance, keep going
-				continue
-		else: #if textchunk isn't marked as a line, keep going
-			continue
-	return line_num #print the line number
 
 def get_context(line): #function to get the text context for search matches from xml
 	context = '' #create empty variable for context
@@ -91,108 +61,72 @@ def get_context(line): #function to get the text context for search matches from
 			context = sentence 
 	return context #print the sting as match context
 
+def make_citation(line, text): #function to get the full citaiton information for search matches using previously made functions
+	cite_item = '' #create empty dicitionary to hold citation info
+
+	text_title = soup.title.string #create variable for text title - needs some refining to grab just text title and not xml title, might be lower in file
+	if soup.author: #if an author is recorded for the file
+		text_author = soup.author.string #create a variable for text author
+	else: 
+		text_author = '' #otherwise record text author as blank
+
+	def get_section(line): #function to get the section citation info from xml
+		cite_list = [] #creates empty list for citation information
+		for parent in line.parents: #for parents of keyword match
+			if parent.name == 'div': #if the parent is a div
+				if parent.attrs['type'] == 'textpart': #and if the div has textpart as an attibute
+					try:
+						cite_list.append(parent.attrs['subtype']+parent.attrs['n']) #grab the subtype (book, chapter, section) and the number
+					except KeyError: #if this doesn't work for one instance, keep going
+						continue
+				else: continue #if the div doesn't have this attribute, keep going
+			elif 'div' in parent.name: #if the the xml encoding is differnt, div might be in the parent name
+				try:
+					cite_list.append(parent.attrs['type']+parent.attrs['n']) #in that case, grab the type and number
+				except KeyError:
+					continue
+		cite_list.reverse() #make the citation order: book,chapter,section
+		citation = '.'.join(cite_list) #make format: book.chapter.section
+		return citation #print the citation
+
+	def get_linenum(line): #separated from get_section to test, function to get the line number citation info from xml
+		textchunk = line.parent #the exact section of .xml text in which the match is found (could be a paragraph or a line)
+		line_num = '' #create an empty variable for the line number
+		for chunk in textchunk: #for each element in textchunk
+			if textchunk.name == 'l' or 'lb': #if the textchunk is marked as a line
+				try:
+					line_num = textchunk.name + ' ' + textchunk['n'] #line number is "line"+number
+				except KeyError: #if this doesn't work for one instance, keep going
+					continue
+			else: #if textchunk isn't marked as a line, keep going
+				continue
+		return line_num #print the line number
+
+	cite_item = text_author + ' , ' + text_title + ' , ' + get_section(line) + '.' + get_linenum(line) + ':' + text
+	return cite_item
+
+def make_jobj(dict1, dict2): #function to create json object from two dictionaries
+	merge_dict = defaultdict(list) #create a defeault dict for combined data
+	for d in (dict1, dict2): #iterate through items in each dictionary
+		for key, value in d.items():
+			merge_dict[key].append(value)
+	return merge_dict
+
+	#still need to merge dicts at right level
+
 # =====================================================================
 # Program
 # =====================================================================
 
 greekcorpusdir = "/Users/stellafritzell/mythodikos/canonical-greekLit-master" #directory with files for text mining
-outfile = "/Users/stellafritzell/mythodikos/corpus-test-10-27.json" #file to write search data, change file type to .json with .json search
+outfile = "/Users/stellafritzell/mythodikos/corpus-test-12-11.json" #file to write search data, change file type to .json with .json search
 
-#list of 1st keywords
-persondict = {
-			'Achilles': [r'\bἈχιλλε', r'\bἈχιλλέ', r'\bἈχιλε', r'\bἈχιλέ'], #may be getting some false results, double check regex
-			'Actaeon': [r'\bἈκταίων', r'\bἈκταίον'],
-			'Adonis': [r'Ἄδωνις', r'Ἄδωνιν', r'\bἈδώνιδ', r'\bἈδωνίδ'], #overlap with festival / month names
-			'Amymone': [r'\bἈμυμώνη', r'\bἈμυμώνῃ', r'\bἈμυμώνα', r'\bἈμυμῶνα', r'\bἈμυμόνη'], #also the name of a river
-			'Andromeda': [r'\bἈνδρομέδα', r'\bἈνδρομέδᾳ', r'\bἈνδρομέδη', r'\bἈνδρομέδῃ'],
-			'Antiope': [r'\bἈντιόπ'],
-			'Arachne': [r'\bἈράχνη', r'\bἈράχνῃ'],
-			'Arion': [r'\bἈρίων', r'\bἈρίον'], #also the name of a horse
-			'Ascalabus': [r'\bἈσκάλαβο'],
-			'Asclepius': [r'\bἈσκληπιό', r'\bἈσκληπιο', r'\bἈσκλήπιο', r'Ἀσκληπιέ', r'\bἈσκλαπ'],
-			'Atalanta': [r'\bἈταλάντ'],
-			'Bellerophon': [r'\bΒελλερεφόντα', r'\bΒελλεροφόντα', r'\bΒελερόφοντα', r'\bΒελλερεφόντη', '\bΒελλερεφόντῃ', r'\bΒελλεροφόντῃ', r'\bΒελλεροφόντη', r'\bΒελεροφόντη', r'\bΒελλεροφόντο', r'\bΒελλερόφοντο', r'\bΒελερόφοντο', r'\bΒελεροφόντο', r'\bΒελλεροφώ', r'\bΒελλεροφῶ'],
-			'Busiris': [r'\bΒούσιρι', r'\bΒουσίριδ'],
-			'Cadmus': [r'Κάδμε', r'\bΚάδμοι', r'\bΚάδμον', r'\bΚάδμος', r'\bΚάδμου', r'\bΚάδμω', r'\bΚάδμῳ'],
-			'Callisto': [r'\bΚαλλιστώ', r'Καλλιστῆς', r'Καλλιστοῖ', r'Καλλιστοῦς'],
-			'Cecrops': [r'\bΚέκροψ', r'\bΚέκρωψ', r'\bΚέκροπο', r'\bΚέκροπα', r'\bΚέκροπε', r'\bΚεκρόπε', r'Κέκροπι'],
-			'Coronis': [r'Κορωνίς', r'\bΚορωνίδα', r'\bΚορωνίδο', r'\bΚορωνίδι'], #there are multiple Coronises
-			'Cycnus': [r'\bΚύκνο', r'\bΚύκνῳ', r'\bΚύκνω', r'Κύκνε'], #there are multiple Cycnuses, typically identified by toponym --> seperation will occur in search/data collection
-			'Cyparissus': [r'\bΚυπάρισσο', r'\bΚυπαρίσσο', r'\bΚυπαρισσο', r'\bΚυπάριτ', r'\bΚυπαρίτ'], #also a city name
-			'Cyrene': [r'\bΚυρήνη', r'\bΚυρήνῃ', r'\bΚυράνα', r'\bΚυράνᾳ'], #also a city name
-			'Danae': [r'\Δανάη', r'\bΔανάῃ', r'\bΔανάα'],
-			'Deucalion': [r'\bΔευκαλίω', r'\bΔευκαλλίω'],
-			'Diomedes': [r'\bΔιομήδη', r'\bΔιομήδῃ', r'\bΔιομήδο', r'\bΔιομήδε', r'\bΔιόμηδε'], #may be getting some false results, overlap with female name Diomede, adjectival use
-			'Endymion': [r'\bἘνδυμίων'],
-			'Erysichthon': [r'\bἘρυσίχθ', r'\bἘρισίχθ'],
-			'Eryx': [r'Ἔρυξ', r'Ἔρυκα', r'\bἘρύκη', r'\bἘρύκῃ', r'\bἜρυκι', r'\bἜρυκο'], #there are multiple Eryxes, also a city name, TLG finds only a feminine name - contrast with male figure in THEOI
-			'Europa': [r'\bΕὐρώπη', r'\bΕὐρώπῃ', r'\bΕὐρώπα', r'\bΕὐρώπᾳ'], #also a place name, may be getting some false results
-			'Euadne': [r'\bΕὐάδν'], #there are multiple Euadnes
-			#'Ganymede': [r'\b'],
-			#'Geryon': [r'\b'],
-			#'Heracles': [r'\b'],
-			'Hippolyta': [r'\bἹππολύτα', r'\bἹππολύτη'],
-			#'Hyacinthus': [r'\b'],
-			#'Iamus': [r'\b'],
-			#'Iasion': [r'\b'],
-			#'Icarius': [r'\b'],
-			#'Io': [r'\b'],
-			#'Ixion': [r'\b'],
-			#'Jason': [r'\b'],
-			#'Leda': [r'\b'],
-			#'Lycaon': [r'\b'],
-			#'Lycurgus': [r'\b'],
-			'Meleager': [r'\bΜελέαγρ', r'\bΜελεάγρ'], #may be getting some false results, overlap with festival name
-			'Midas': [r'\bΜίδα', r'\bΜίδᾳ', r'\bΜίδη', r'Μίδου', r'Μίδῃ', r'Μίδεω', r'\bΜῖδα'],
-			#'Minyades': [r'\b'], #a trio of princesses
-			'Narcissus': [r'\bΝάρκισσο', r'\bΝαρκίσσο', r'Ναρκίσσῳ', r'\bΝάρκισο', r'\bΝαρκίσο'],
-			#'Odysseus': [r'\b'],
-			#'Oedipus': [r'\b'],
-			#'Orestes': [r'\b'],
-			#'Orion': [r'\b'],
-			#'Otrera': [r'\b'],
-			#'Pandora': [r'\b'],
-			'Pasiphae': [r'\bΠασιφάη', r'\bΠασιφάῃ', r'\bΠασιφάα'],
-			'Pelops': [r'\bΠέλο'], #accent should limit results in regex
-			#'Penthesilea': [r'\b'],
-			#'Perseus': [r'\b'],
-			#'Phaethon': [r'\b'],
-			#'Psyche': [r'\b'],
-			#'Pygmalion': [r'\b'],
-			#'Pyrrha': [r'\b'],
-			#'Salmoneus': [r'\b'],
-			#'Sisyphus': [r'\b'],
-			#'Tantalus': [r'\b'],
-			#'Tennes': [r'\b'],
-			#'Theseus': [r'\b'],
-			#'Tithonus': [r'\b'],
-			#'Triptolemus': [r'\b'],
-			#'Tyro': [r'\b']
-			} # Compile in reference to https://www.theoi.com/greek-mythology/heroes.html ; eventually LIMC
-
-#list of 2nd keywords - eventually add lowercase reg-ex for adjectival / toponymical uses
-placedict = {
-			'Arcadia': [r'\bἈρκαδ', r'\bἈρκάδ'],
-			'Boeotia': [r'\bΒοιωτ', r'\bΒοιῳτ', r'\bΒοϊωτ', r'\bΒοέκ', r'\bΒοεκ'],
-			'Calydon': [r'\bΚαλυδόν', r'\bΚαλύδων', r'\bΚαλυδῶν', r'\bΚαλυδών'],
-			'Colchis': [r'\bΚόλκ', r'\bΚολκ', r'\bΚολχ'],
-			'Corinth': [r'\bΚόρινθ', r'\bΚορίνθ', r'\bΚορινθ'],
-			'Lacedaemon': [r'\bΛακεδ', r'\bΛαβεδ', r'\bΛακκοδ', r'\bΛακηδ', r'\bΛακιδ', r'\bΛακοδ', r'\bΛαχεδ'],
-			'Lesbos': [r'\bΛέσβε', r'\bΛέσβη', r'\bΛέσβο', r'\bΛέσβῳ', r'\bΛέσβω'],
-			#'Lycaeus': [r''],
-			#'Mainalos': [r''],
-			#'Methymna': [r''],
-			#'Parthenion': [r''],
-			#'Taenarum': [r''],
-			#'Tarentum': [r''],
-			#'Tegea': [r''],
-			'Thebes': [r'\bΘήβ', r'\bΘῆβ']
-			} # Compile in reference to filtered Pleiades data, TLG lemma search for varient Greek spellings
-
-#build json (#csv) file with desired search metadata
+#build json file with desired search metadata
 with open(outfile, 'w') as z: #to access/create the outfile for data from text-mining
-	#f = csv.writer(z)
-	#f.writerow(['person', 'place', 'title', 'author', 'section', 'line', 'context', 'length', 'filename'])
+
+	citations = defaultdict(list) #create default dict to collect citations for person-place pairs in list class
+	pair_data = {} # new dict for other stuff on ids 
+	# OR, because I can't figure out how to accurately combine these... create seperate dict for each unique key needed for geojson and then combine using default dict
 
 	for root, dirs, files in os.walk(greekcorpusdir):
 		for file in files:
@@ -202,35 +136,40 @@ with open(outfile, 'w') as z: #to access/create the outfile for data from text-m
 				with open(infile) as x: #open and iterate through each identifed file
 					soup = BeautifulSoup(x, features='lxml')
 
-					text_title = soup.title.string #create variable for text title
-					if soup.author: #if an author is recorded for the file
-						text_author = soup.author.string #create a variable for text author
-					else: text_author = '' #otherwise record text author as blank
-
-					# could I insert a 'with open(file)' command here to access a seperate person-dict file?
 					for person in persondict:
+						#citations = []
 						per_terms = persondict[person] # value list for each person key
 						for per in per_terms: # each regex value
 							per_matches = soup.find_all(string=re.compile(per)) #using soup.find_all in place of re.search for navigating xml
 							for per_match in per_matches:
 								context = get_context(per_match)
 
-								# could I insert a 'with open(file)' command here to access a seperate place-dict file?
 								for place in placedict:
-									pl_terms = placedict[place] # value list for each place key
-									for pl in pl_terms: # earch regex value
-										pair = re.search(pl, context) # searches context for x
-										if pair:
-											try:
-												section = get_section(per_match)
-												line = get_linenum(per_match)
-												length = len(context) # number of characters
-												#f.writerow([person, place, text_title, text_author, section, line, context, length, file]) # write defined variables for matches to csv
-												data = {person + '-' + place: {'person': person, 'place': place, 'citation': [text_author, text_title, section, line], 'text': context, 'length': length, 'file': file}}
-												json.dump(data, z, indent=4)
-											except KeyError:
-												continue
+									pl_keys = placedict[place] # list of keys for each place item
+									pl_terms = pl_keys['spellings'] # values under specificed key
+									pl_id = pl_keys['pleiades id']
+									pl_gps = pl_keys['coordinates']
+									for term in pl_terms: # search regex value
+										pair = re.search(term, context) # searches context for term
+										if pair: #put combined function for citation here instead of all factors, comment out citation list above
+											citation = make_citation(per_match, context)
 										else:
 											continue
 
-# write code to attach geo coordinates to dataset from Pleiades .csv or .json file
+										ID = person +'-'+ place #unique identifier for match pair
+										citations[ID].append(citation) #in dict creates key for ID (if new) and appends citation as value in list
+										
+										pair_data[ID] = {
+											'type': 'Feature', 
+											'title': place, 
+											'geometry': {'type': 'Point', 'coordinates': pl_gps}, 
+											'properties': {'pleiades id': pl_id, 'citations': []}
+											}
+	
+	#print(citations)
+	entries = make_jobj(citations, pair_data)
+	#print(entries)
+
+	#zip dictionaries on uinique ids, make_jobj() function? to merge values at the right level
+	json.dump(entries, z, indent=4) #writes entries to json outfile, use json.dumps to continue work within program
+
